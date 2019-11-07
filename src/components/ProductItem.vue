8d4854f1b63efef61014dd96b3ff97959cdc89b4
Ellanski <nameimprobable@gmail.com>
2019-11-07 21:20:08 +0300

    scaffold

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <details :open="isOpen">
+    <summary>{{ product.name }}</summary>
+    <div>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  </details>
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ProductItem/>
+  </div>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem.vue';
+
+export default {
+  name: 'home',
+  components: {
+    ProductItem,
+  },
+};
+</script>
