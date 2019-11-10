409e64e7416dcf6bc660df8db0378deeb7b19886
Ellanski <nameimprobable@gmail.com>
2019-11-10 18:54:34 +0300

    localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 import products from '@/store/products';
 import cart from '@/store/cart';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // храним корзину на клиенте (заодно и остальное, а то апиха полумёртвая)
+  plugins: [createPersistedState()],
   modules: {
     products,
     cart,
