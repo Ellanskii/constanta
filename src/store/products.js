1eb3c1fc9002febba62d201f05c57e0dd084789d
Ellanski <nameimprobable@gmail.com>
2019-11-10 20:04:16 +0300

    getting all products simultaneously

--- src/store/products.js
+++ src/store/products.js
@@ -5,13 +5,43 @@ const getters = {
 };
 
 const actions = {
-  async getProducts({ commit }) {
-    await axios
-      .get('https://swapi.co/api/starships')
-      .then((res) => {
-        commit('SET_PRODUCTS', res.data.results);
+  async getProducts({ commit }, pagination = false) {
+    let products = [];
+    let pagesCount = 1;
+    function getPartialData(page = 1) {
+      return new Promise((resolve, reject) => {
+        axios
+          .get(`https://swapi.co/api/starships/?page=${page}`)
+          .then((res) => {
+            resolve(res.data);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    }
+
+    // получаем первую пачку, смотрим сколько товаров всего
+    await getPartialData()
+      .then((data) => {
+        pagesCount = Math.ceil(data.count / 10);
+        products = products.concat(data.results);
       })
-      .catch();
+      .catch(console.error);
+
+    if (!pagination || pagesCount === 1) { // Не все товары уместились в один запрос
+      const API_CALLS = [];
+      for (let page = 2; page <= pagesCount; page += 1) {
+        API_CALLS.push(getPartialData(page));
+      }
+      await Promise.all(API_CALLS).then((responses) => {
+        responses.forEach((response) => {
+          products = products.concat(response.results);
+        });
+      }).catch(console.error);
+    }
+
+    commit('SET_PRODUCTS', products);
   },
 };
 
