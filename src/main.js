b651dc01addbd0c8892db123305cae7e1db965bb
Unknown <nameimprobable@gmail.com>
2019-11-11 17:42:14 +0300

    metrika

--- src/main.js
+++ src/main.js
@@ -8,12 +8,19 @@ import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 // eslint-disable-next-line
 import brands from '@fortawesome/fontawesome-free-brands';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import VueYandexMetrika from 'vue-yandex-metrika';
 import store from './store';
 import router from './router';
 import App from './App.vue';
 
 library.add(faAngleDown, brands);
 Vue.component('FaIcon', FontAwesomeIcon);
+
+Vue.use(VueYandexMetrika, {
+  id: 56179357,
+  router,
+  env: process.env.NODE_ENV,
+});
 
 Vue.config.productionTip = false;
 
